65aaca1c19dde4f4ecf690d840b00434c4f86f90
Mariana1010P <portela.mariana.6654@eam.edu.co>
2024-09-10 20:53:13 -0500

    add project

---
+++ main.py
@@ -0,0 +1,15 @@
+
+"""
+Sum of two numbers
+"""
+def suma(a, b):
+    """
+    Calculate the sum of two numbers
+    Args:
+        a (int or float): first number 
+        b (int or float): second number  
+
+    Returns:
+        int: sum of a and b
+    """
+    return a + b
